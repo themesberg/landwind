d2c9a0311cffa95b1228f6eb9763269f720810f7
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-07-05 14:35:33 +0300

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
